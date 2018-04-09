9b3c9e7277e45c4dfaed077a7a10b4e123e51edd
Marcela <marcelaflopez@gmail.com>
2018-04-09 14:10:01 -0300

    Prueba para compartir. Primera versión

---
+++ Pepito_escritorio/Pepito_escritorio/UserControl1.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Marcela López
+ * Date: 28/7/2017
+ * Time: 11:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pepito_escritorio
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
